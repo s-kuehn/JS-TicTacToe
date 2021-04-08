ab4d998393fe098dde44e09db12624993739cd15
unknown <amazingsdk@gmail.com>
2021-04-08 13:42:09 -0400

    Started on JS logic

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 'use strict'
 
-console.log('TEST!!!');
+// Initialize Variables
+const topLeftBox = document.querySelector('#top-left');
+const topMidBox = document.querySelector('#top-mid');
+const topRightBox = document.querySelector('#top-right');
+
+const midLeftBox = document.querySelector('#mid-left');
+const midMidBox = document.querySelector('#mid-mid');
+const midRightBox = document.querySelector('#mid-right');
+
+const botLeftBox = document.querySelector('#bot-left');
+const botMidBox = document.querySelector('#bot-mid');
+const botRightBox = document.querySelector('#bot-right');
+
+// Check if game is over
+// Check if box is empty
+// Rotate between "X"s and "O"s every other turn
+// Start with "X"s
