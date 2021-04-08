47d89cd637ebb8959c7c08f5caab0bffb3a189f9
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-04-08 17:00:04 -0400

    board variable with 2d array added

--- script.js
+++ script.js
@@ -18,18 +18,27 @@ const boxes = [topLeftBox, topMidBox, topRightBox, midLeftBox,
 
 let isXTurn = true;
 
-
-function toggleTurn (x,o) {
+function toggleTurn (x,o,location) {
     if (isXTurn) {
+        board[location[0]][location[1]] = 'x'
         x.style.display = 'block';
         o.style.display = 'none';
         isXTurn = false;
        } else {
+        board[location[0]][location[1]] = 'o'
         o.style.display = 'block';
         x.style.display = 'none';
         isXTurn = true;
        }
 }
+
+const board = [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+]
+
+
 
 // Winning Conditions
 const winningConditions = [
@@ -47,47 +56,56 @@ const winningConditions = [
 topLeftBox.addEventListener('click', ()=> {
     let x = topLeftBox.querySelector('.x')
     let o = topLeftBox.querySelector('.o')
-    toggleTurn(x,o);
+    toggleTurn(x,o,[0,0]);
+    console.log(board)
 })
 topMidBox.addEventListener('click', ()=> {
     let x = topMidBox.querySelector('.x')
     let o = topMidBox.querySelector('.o')
-    toggleTurn(x,o);
+    toggleTurn(x,o,[0,1]);
+    console.log(board)
 })
 topRightBox.addEventListener('click', ()=> {
     let x = topRightBox.querySelector('.x')
     let o = topRightBox.querySelector('.o')
-    toggleTurn(x,o);
+    toggleTurn(x,o,[0,2]);
+    console.log(board)
 })
 midLeftBox.addEventListener('click', ()=> {
     let x = midLeftBox.querySelector('.x')
     let o = midLeftBox.querySelector('.o')
-    toggleTurn(x,o);
+    toggleTurn(x,o,[1,0]);
+    console.log(board)
 })
 midMidBox.addEventListener('click', ()=> {
     let x = midMidBox.querySelector('.x')
     let o = midMidBox.querySelector('.o')
-    toggleTurn(x,o);
+    toggleTurn(x,o,[1,1]);
+    console.log(board)
 })
 midRightBox.addEventListener('click', ()=> {
     let x = midRightBox.querySelector('.x')
     let o = midRightBox.querySelector('.o')
-    toggleTurn(x,o);
+    toggleTurn(x,o,[1,2]);
+    console.log(board)
 })
 botLeftBox.addEventListener('click', ()=> {
     let x = botLeftBox.querySelector('.x')
     let o = botLeftBox.querySelector('.o')
-    toggleTurn(x,o);
+    toggleTurn(x,o,[2,0]);
+    console.log(board)
 })
 botMidBox.addEventListener('click', ()=> {
     let x = botMidBox.querySelector('.x')
     let o = botMidBox.querySelector('.o')
-    toggleTurn(x,o);
+    toggleTurn(x,o,[2,1]);
+    console.log(board)
 })
 botRightBox.addEventListener('click', ()=> {
     let x = botRightBox.querySelector('.x')
     let o = botRightBox.querySelector('.o')
-    toggleTurn(x,o);
+    toggleTurn(x,o,[2,2]);
+    console.log(board)
 })
 
 document.querySelector('.new-game').addEventListener('click', ()=> {
