bc2ea873a8b5a5edc75506cc693cf53ed70b0089
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-04-08 13:48:15 -0400

    click listeners added to each game box

--- script.js
+++ script.js
@@ -13,6 +13,35 @@ const botLeftBox = document.querySelector('#bot-left');
 const botMidBox = document.querySelector('#bot-mid');
 const botRightBox = document.querySelector('#bot-right');
 
+// Click Listeners
+topLeftBox.addEventListener('click', ()=> {
+    alert('clicked');
+})
+topMidBox.addEventListener('click', ()=> {
+    alert('clicked');
+})
+topRightBox.addEventListener('click', ()=> {
+    alert('clicked');
+})
+midLeftBox.addEventListener('click', ()=> {
+    alert('clicked');
+})
+midMidBox.addEventListener('click', ()=> {
+    alert('clicked');
+})
+midRightBox.addEventListener('click', ()=> {
+    alert('clicked');
+})
+botLeftBox.addEventListener('click', ()=> {
+    alert('clicked');
+})
+botMidBox.addEventListener('click', ()=> {
+    alert('clicked');
+})
+botRightBox.addEventListener('click', ()=> {
+    alert('clicked');
+})
+
 // Check if game is over
 // Check if box is empty
 // Rotate between "X"s and "O"s every other turn
