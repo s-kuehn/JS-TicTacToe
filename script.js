3a4d5a392bb38449481396f90a75d315b813fc8e
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-04-08 14:21:55 -0400

    new game btn centered with flexbox

--- script.js
+++ script.js
@@ -16,6 +16,18 @@ const botRightBox = document.querySelector('#bot-right');
 let isXTurn = false;
 let isOTurn = false;
 
+// Winning Conditions
+const winningConditions = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8]
+]
+
 // Click Listeners
 topLeftBox.addEventListener('click', ()=> {
     let div = document.createElement('div');
